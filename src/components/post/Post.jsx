dbf3765f2cbd73b05dd5007c0bd32779c9d9c9b5
AJPascoe <pascoe312@gmail.com>
2021-09-29 22:55:23 +0100

    post component and CSS

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,39 @@
+import "./post.css";
+import {MoreVert} from "@material-ui/icons"
+
+export default function Post() {
+    return (
+        <div className="post">
+            <div className="postWrapper">
+                <div className="postTop">
+                    <div className="postTopLeft">
+                        <img className="postProfileImg" src="/assets/person/2.jpeg" alt="" />
+                        <span className="postUsername">Steve Gary</span>
+                        <span className="postDate">1 hour ago</span>
+                    </div>
+                    <div className="postTopRight">
+                        <MoreVert/>
+                    </div>
+                </div>
+                <div className="postCenter">
+                    <span className="postText">Hey guys, jumping on my first train in Tokyo!</span>
+                    <img className="postImg" src="assets/post/1.jpeg" alt="" />
+                </div>
+                <div className="postBottom">
+                    <div className="postBottomLeft">
+                        <img className="likeIcon" src="assets/like.png" alt="" />
+                        <img className="likeIcon" src="assets/heart.png" alt="" />
+                        <span className="postLikeCounter">22 people like it</span>
+
+                    </div>
+                    <div className="postBottomRight">
+                        <span className="postCommentText">10 comments</span>
+                    </div>
+
+                </div>
+                
+            </div>
+       
+        </div>
+    )
+}
